eced9b850aaf6815261e41fcf3a4dca0259d13dc
22-Pig <845501558@qq.com>
2022-05-05 11:34:44 +0800

    tree前

---
+++ stack/LinkStack.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType; // 定义ElemType为int类型
+
+typedef struct StackNode // 定义链栈结点类型
+{
+    ElemType data;          // 数据域
+    struct StackNode *next; // 指针域
+} StackNode, *LinkStack;    // 栈类型定义
+
+/* 初始化一个链栈 (不带头结点) */
+bool InitLinkStack(LinkStack &S)
+{
+    S = NULL;
+    return true;
+}
+
+/* 栈判空 */
+bool StackEmpty(LinkStack L)
+{
+    if (L == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+/* 入栈 */
+bool Push(LinkStack &S, ElemType e)
+{
+
+    StackNode *p = (StackNode *)malloc(sizeof(StackNode));
+    p->data = e;
+    p->next = S;
+    S = p;
+    return true;
+}
+
+/* 出栈 */
+bool Pop(LinkStack &S, ElemType &e)
+{
+    if (S == NULL)
+        return false; // 链栈空
+
+    StackNode *p = S;
+    e = S->data;
+    S = S->next;
+    free(p);
+    return true;
+}
+
+/* 读取栈顶元素 */
+bool GetTop(LinkStack S, ElemType &x)
+{
+    if (S == NULL)
+    {
+        return false; // 栈空，报错
+    }
+    StackNode *p = S;
+    x = S->data; // x记录栈顶元素
+    free(p);
+    return true;
+}
+
+/* 遍历输出栈 */
+void printStack(LinkStack S)
+{
+    printf("栈顶->");
+    StackNode *p = S;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    LinkStack S;      // 声明一个指向链栈的指针
+    InitLinkStack(S); // 初始化一个空链栈
+    int e, x;
+    printf("请输入一个要入栈的元素（-1表示结束）：");
+    scanf("%d", &e);
+    while (e != -1)
+    {
+        Push(S, e);
+        printf("请输入一个要入栈的元素（-1表示结束）：");
+        scanf("%d", &e);
+    }
+    printStack(S);
+
+    printf("出栈测试：");
+    Pop(S, e);
+    printStack(S);
+
+    printf("取栈顶元素测试：");
+    GetTop(S, x);
+    printf("取出的栈顶元素为：%d\n", x);
+}
---
+++ stack/SqStack.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MaxSize 10
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType data[MaxSize]; // 静态数组存放栈中元素
+    int top;                // 栈顶指针
+} SqStack;
+
+/* 初始化栈 */
+void InitStack(SqStack &S)
+{
+    S.top = -1; // 初始化栈顶指针
+}
+
+bool StackEmpty(SqStack S)
+{
+    if (S.top == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/* 新元素入栈 */
+bool Push(SqStack &S, ElemType x)
+{
+    if (S.top == MaxSize - 1)
+    {
+        return false; // 栈满，报错
+    }
+    // S.data[++S.top] = x;
+    S.top = S.top + 1; // 指针先加1
+    S.data[S.top] = x; // 新元素入栈
+    return true;
+}
+
+/* 出栈操作 */
+bool Pop(SqStack &S, ElemType &x)
+{
+    if (S.top == -1)
+    {
+        return false; // 栈空，报错
+    }
+    // x = S.data[S.top]--;
+    x = S.data[S.top]; // 栈顶元素先出栈
+    S.top = S.top - 1; // 指针减一
+    return true;
+}
+
+/* 读取栈顶元素 */
+bool GetTop(SqStack S, ElemType &x)
+{
+    if (S.top == -1)
+    {
+        return false; // 栈空，报错
+    }
+    x = S.data[S.top]; // x记录栈顶元素
+    return true;
+}
+
+void test()
+{
+    SqStack S; // 声明一个顺序栈(分配空间)
+    InitStack(S);
+}
